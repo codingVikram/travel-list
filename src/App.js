3f616fdb94ac83dab4407aabb0026c36375fa02e
codingVikram <multani_v@hotmail.com>
2024-02-23 18:48:28 +0000

    app component rendered

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App() {
+  <div className="app">
+    <Logo />
+    <Form />
+    <PackingList />
+    <Stats />
+  </div>;
+}
 
-function App() {
+function Logo() {
+  return <h1> 🏝️ Far Away 🧳</h1>;
+}
+function Form() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
     </div>
   );
 }
-
-export default App;
+function PackingList() {
+  return <div className="list"></div>;
+}
+function Stats() {
+  return (
+    <footer>
+      <em>💼 You have X items on your list and you already packed X (x%)</em>
+    </footer>
+  );
+}
