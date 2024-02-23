07bed146ed9b8a99304190d7c5d68c987b9c60c7
codingVikram <multani_v@hotmail.com>
2024-02-23 20:49:46 +0000

    form added

--- src/App.js
+++ src/App.js
@@ -19,10 +19,23 @@ function Logo() {
   return <h1> 🏝️ Far Away 🧳</h1>;
 }
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="item..." />
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
@@ -30,7 +43,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
